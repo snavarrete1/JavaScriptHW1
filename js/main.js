a6ea133e6c6be30c533f8861e6050ddec3c4eb9b
snavarrete1 <snavarrete3077@gmail.com>
2020-08-24 18:22:35 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(a,b){
+    for (let i= 0; i < b.length; i++){
+        console.log(b[i])
+        if (a.search(b[i])){
+            console.log("Matched " + b[i])
+        } else {console.log('No Matches')}
+    }
+    
+}
+console.log(findWords(dog_string,dog_names));
+
+
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+let given_arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    for ( let i = 0; i < arr.length; i++) {
+        if( i % 2 == 0){
+            arr.splice(i,1,'even index')
+        }
+    }
+    return arr
+};
+console.log(replaceEvens(given_arr))
+
+
+/* codewars */
+let Nums= function(){
+    let num = 6;
+    let num2 = 18;
+    let num3 = 20;
+
+    return num + num3 * num2
+}
+console.log(Nums())
+
+
+function greet(name) {
+    console.log("Hello, " + name + "how are you doing today?");
+  }
+  
+  greet("Stephanie");
+
+console.log(greet)
